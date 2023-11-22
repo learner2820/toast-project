6ce16fd630f9808304d89b8c1558b73809b26630
learner2820 <padup1401@chmail.ir>
2023-11-22 03:05:44 -0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Footer from './components/Footer'
+import ToastPlayground from './components/ToastPlayground'
+
+function App() {
+
+  return (
+    <>
+      <ToastPlayground />
+      < Footer/>
+    </>
+  )
+}
+
+export default App
